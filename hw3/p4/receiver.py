Fixed normalization issue

hw3/p4/receiver.py
@@ -70,6 +70,6 @@ def receiver(recvd: np.ndarray, preamble: np.ndarray, bits_per_symbol: int=1, am
     if (x < 1 or y < 1) or (len(out) < x * y):
         return None, index
     out = out[:x*y].astype(np.uint8)
-    out = out.reshape((x, y))  
+    out = out.reshape((x, y))
     
     return out, index

hw3/p4/test_main.py
@@ -26,7 +26,11 @@ def calculate_error_rate(arr1: np.ndarray, arr2: np.ndarray, bits_per_symbol: in
     """
     assert(bits_per_symbol > 0)
     assert(arr1.shape == arr2.shape)
-    return np.sum(np.bitwise_count(arr1 - arr2)) / (arr1.size * bits_per_symbol)
+    diff = arr1 - arr2
+    reshaped = diff.reshape(-1, 4)  # 16-QAM has 4 bits
+    bit_err = np.sum(np.bitwise_count(diff)) / (arr1.size * bits_per_symbol)
+    sym_err = np.sum(np.any(reshaped != 0, axis=1)) / len(reshaped)
+    return bit_err, sym_err
 
 PREAMBLE = np.array((1, 0, 1, 0, 1, 1, 1, 1) * 4)
 bits_per_symbol = 1
@@ -35,22 +39,21 @@ snr = 100  # in dB
 
 # get image
 test_input = image_to_bits('./photos/monalisa_diff.png')
-# print("Input image:", test_input)
+print("Input image:", test_input)
 
 # transform image
 transmitter_output = transmitter(test_input, preamble=PREAMBLE, symbol_size=symbol_size, bits_per_symbol=bits_per_symbol)
-# print("Transformer output:", transmitter_output)
+print("Transformer output:", transmitter_output)
 # add zeros before and after data
 r = round(np.random.rand() * 6e5)
 signal = np.concatenate((np.zeros(r), transmitter_output, np.zeros(int(6e5-r))))
-# print("Padded signal:", signal)
+print("Padded signal:", signal)
 
 # make sure it's the right length
 assert len(signal) == int(1e6), f"{len(signal)}"
 
 # add noise
 noisy_output = truncate_add_noise_passband(signal, snr)
-noisy_output = signal
 print("Noisy signal:", noisy_output)
 
 # receive signal
@@ -61,58 +64,74 @@ if index != r:
 if received_signal is None:
     print("Unable to display image")
     
-# print("Received signal:", received_signal)
+print("Received signal:", received_signal)
 
 plt.imshow(test_input, cmap='gray', vmin=0, vmax=1)
-plt.title("Original Signal")
 plt.show()
 
 if received_signal is not None:
     plt.imshow(received_signal, cmap='gray', vmin=0, vmax=1)
-    # plt.title("Received Signal")
     plt.show()
-    print("Error rate:", calculate_error_rate(test_input, received_signal))
 
-snrs = range(-10, 21, 1)
+N = 3
+snrs = range(0, 21, 2)
 det_rates = []
 dec_rates = []
+sers = []
+bers = []
 for test_snr in snrs:
     print(f"Starting with SNR = {test_snr}")
-    detected = 100
-    decoded = 100
-    for _ in range(100):
+    detected = N
+    decoded = N
+    ber = 0
+    ser = 0
+    for _ in range(N):
         # add zeros before and after data
-        r = round(np.random.rand() * 9e5)
-        signal = np.concatenate((np.zeros(r), transmitter_output, np.zeros(int(9e5-r))))
+        r = round(np.random.rand() * 6e5)
+        signal = np.concatenate((np.zeros(r), transmitter_output, np.zeros(int(6e5-r))))
         noisy_output = truncate_add_noise_passband(signal, test_snr)
-        received_signal, index = receiver(noisy_output, preamble=PREAMBLE, bits_per_symbol=bits_per_symbol)
+        received_signal, index = receiver(noisy_output, preamble=PREAMBLE, bits_per_symbol=bits_per_symbol, symbol_size=symbol_size)
         if index != r:
             print("index wrong")
             print(index, r)
             detected -= 1
             decoded -= 1
+            ber += 1
+            ser += 1
             continue
         if received_signal is None:
             decoded -= 1
+            ber += 1
+            ser += 1
             continue
         if test_input.shape != received_signal.shape:
             decoded -= 1
+            ber += 1
+            ser += 1
             continue
-        if calculate_error_rate(test_input, received_signal) != 0:
+        this_ber, this_ser = calculate_error_rate(test_input, received_signal)
+        print(this_ber, this_ser)
+        if this_ber != 0:
             decoded -= 1
-    det_rates.append(detected / 100)
-    dec_rates.append(decoded / 100)
+        ber += this_ber
+        ser += this_ser
+    det_rates.append(detected / N)
+    dec_rates.append(decoded / N)
+    bers.append(ber / N)
+    sers.append(ser / N)
 
 plt.title(f"SNR vs. Error Rates")
 plt.xlabel("SNR (dB)")
 plt.ylabel("Rate")
-plt.plot(snrs, det_rates, label="Detecting Rate")
-plt.plot(snrs, dec_rates, label="Decoding Rate")
+plt.semilogy(snrs, det_rates, label="Detecting Rate")
+plt.semilogy(snrs, dec_rates, label="Decoding Rate")
 plt.legend()
 plt.show()
 
-"""
-iii) The difference is about 8dB.  This exists because if you do not decode the preamble correctly, you cannot decode the rest of the message correcly.
-No necessarily, there will be a difference.  Addiitionally, if you can get the preamble correct, you are still not garunteed to get the rest of the message correct.
-The preamble only represents a very small part of the bits that could possibly have errors.
-"""
+plt.title(f"SNR vs. Error Rates")
+plt.xlabel("SNR (dB)")
+plt.ylabel("Rate")
+plt.semilogy(snrs, bers, label="Bit Error Rate")
+plt.semilogy(snrs, sers, label="Symbol Error Rate")
+plt.legend()
+plt.show()

hw3/p4/truncate_add_noise_passband.py
@@ -18,7 +18,8 @@ def truncate_add_noise_passband(signal, SNR_dB):
     """
 
     # Normalize signal
-    signal = signal / np.max(np.abs(signal))
+    max_sgnl = np.max(np.abs(signal))
+    signal = signal / max_sgnl
 
     # Truncate to 1e6 samples
     N = int(1e6)
@@ -40,4 +41,6 @@ def truncate_add_noise_passband(signal, SNR_dB):
     added_noise = noise  # awgn(reference) - reference
     noisySignal = signal + added_noise
 
+    # unnormalize
+    noisySignal *= max_sgnl
     return noisySignal